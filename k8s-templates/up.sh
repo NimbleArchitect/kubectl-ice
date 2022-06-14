5ab3d9663a4f7bba5ad940ee0f8a137508ddbc03
NimbleArchitect <56797391+NimbleArchitect@users.noreply.github.com>
2022-06-14 16:17:42 +0100

    removed namespace from templates

--- k8s-templates/up.sh
+++ k8s-templates/up.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 kubectl apply -f ./namespace.yml
-kubectl apply -f ./configmap.yml
-kubectl apply -f ./demo-pod.yml
-kubectl apply -f ./demo-memory.yml
-kubectl apply -f ./demo-odd-cpu.yml
-kubectl apply -f ./demo-random-cpu.yml
-kubectl apply -f ./demo-probe.yml
+kubectl apply -n cpu-demo -f ./configmap.yml
+kubectl apply -n single-pods -f ./configmap.yml
+kubectl apply -n resource-demo -f ./configmap.yml
+# kubectl apply -n  -f ./configmap.yml
+# kubectl apply -n cpu-demo -f ./configmap.yml
+
+kubectl apply -n single-pods -f ./demo-pod.yml
+kubectl apply -n resource-demo -f ./demo-memory.yml
+kubectl apply -n resource-demo -f ./demo-odd-cpu.yml
+kubectl apply -n cpu-demo -f ./demo-random-cpu.yml
+kubectl apply -n single-pods -f ./demo-probe.yml
