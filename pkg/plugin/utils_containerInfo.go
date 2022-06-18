70e1ccced38139bdc47f0e30bb297f4d332669a8
NimbleArchitect <56797391+NimbleArchitect@users.noreply.github.com>
2022-06-18 18:01:52 +0100

    added comments to utils_containerInfo

--- pkg/plugin/utils_containerInfo.go
+++ pkg/plugin/utils_containerInfo.go
@@ -2,6 +2,7 @@ package plugin
 
 import v1 "k8s.io/api/core/v1"
 
+// holds a set of columns that are common to every subcommand
 type containerInfomation struct {
 	podName       string
 	containerName string
@@ -10,12 +11,14 @@ type containerInfomation struct {
 	nodeName      string
 }
 
+// GetDefaultHead: returns the common headers in order
 func (ci *containerInfomation) GetDefaultHead() []string {
 	return []string{
 		"T", "NAMESPACE", "NODE", "PODNAME", "CONTAINER",
 	}
 }
 
+// GetDefaultCells: returns an array of cells prepopulated with the common information
 func (ci *containerInfomation) GetDefaultCells() []Cell {
 	return []Cell{
 		NewCellText(ci.containerType),
@@ -26,12 +29,14 @@ func (ci *containerInfomation) GetDefaultCells() []Cell {
 	}
 }
 
+// LoadFromPod: the common information is read from a given pod object and stored internally
 func (ci *containerInfomation) LoadFromPod(pod v1.Pod) {
 	ci.podName = pod.Name
 	ci.namespace = pod.Namespace
 	ci.nodeName = pod.Spec.NodeName
 }
 
+// SetVisibleColumns: sets the visable columns based on properties from flags
 func (ci *containerInfomation) SetVisibleColumns(table Table, flags commonFlags) {
 	if !flags.showNamespaceName {
 		table.HideColumn(1)
